d293e0b07c7903ce71f05b53fad38dd6599c3eb0
sarahmelo <sarahcbdg@gmail.com>
2020-10-26 17:41:28 -0300

    Page styling|Events: Display of value(input)

---
+++ public/js/tasks.js
@@ -0,0 +1,34 @@
+let group = [];
+let index = "";
+
+function add() {
+  let textScreen = window.document.querySelector("#textbox");
+  text = window.document.querySelector("#search").value;
+  input = document.createElement("input")
+  //let text = document.createElement("text")
+  //var addP = window.document.getElementById('primor')
+  //length: usado normalmente para texto
+  //value:
+  if (text.length) {
+    group.push(text);
+    console.log(group);
+
+    index = group.findIndex((item) => item == text);
+    textInScreen(index);
+  }
+
+  function textInScreen(index) {
+    textScreen.innerHtml = ``;
+    console.log("limpei");
+    let element = group[index];
+    textScreen.innerHTML += `${element} <br>`;
+    text.value = ``;
+    // if(array.length > 0) {
+    //   for (let i = 0; i < array.length; i++) {
+    //     const element = array[i];
+    //     textScreen.innerHTML += `${element} <br>`
+    //     text.value= ``
+    //   }
+    // }
+  }
+}
